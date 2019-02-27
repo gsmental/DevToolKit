4fe2dee409bfed7ecea774a52a93f495fbb76af9
Gurpreet Singh Menntal <gsmental@gmail.com>
2019-02-27 08:15:34 +0530

    Add files via upload

---
+++ Xamarin.Android/WebViewWithNativeAccess/AnnoucementsFragment.cs
@@ -0,0 +1,38 @@
+﻿using Android.OS;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using PtuDocsMvvm.JSReceiver;
+using PtuDocsMvvm.Share.ViewModels;
+
+namespace PtuDocsMvvm.Fragements
+{
+    public class AnnoucementsFragment : BaseFragment<AnnoucementsViewModel>
+    {
+        protected override int FragmentId => Resource.Layout.AnnoucementsFragment;
+
+        public static AnnoucementsFragment NewInstance() => new AnnoucementsFragment { Arguments = new Bundle() };
+
+
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.AnnoucementsFragment, container, false);
+
+            WebView webView;
+            webView = view.FindViewById<WebView>(Resource.Id.webView);
+            webView.SetWebChromeClient(new WebChromeClient());
+            webView.Settings.DomStorageEnabled = true;
+            webView.Settings.JavaScriptEnabled = true;
+            webView.AddJavascriptInterface(new JSReceiverAnnoucements(Context), "JSRAnnoucements");
+            webView.LoadUrl(this.ViewModel.WebUrl);
+            return view;
+        }
+    }
+}
---
+++ Xamarin.Android/WebViewWithNativeAccess/JSReceiverAnnoucements.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using Java.Interop;
+using PtuDocsMvvm.Fragements;
+using PtuDocsMvvm.Plugins;
+
+namespace PtuDocsMvvm.JSReceiver
+{
+    public class JSReceiverAnnoucements : Java.Lang.Object
+    {
+        Context _context;
+        private TaskPlugin _taskPlugin = new TaskPlugin();
+
+        public JSReceiverAnnoucements(Context context)
+        {
+            _context = context;
+        }
+
+        //public JSReceiverAnnoucements(Context context, TaskPlugin taskPlugin)
+        //{
+        //    _context = context;
+        //    _taskPlugin = taskPlugin;
+        //}
+
+        [Export]//[Export("ShowToast")]
+        [JavascriptInterface]
+        public void DownloadFile(string Message)
+        {
+            _taskPlugin.ShowToast(Message);
+        }
+    }
+}
